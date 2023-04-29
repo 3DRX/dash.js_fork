d6618f6cf439a78d1f928187485a912b53d2830f
3DRX <kjy20030228@icloud.com>
2023-04-30 00:24:53 +0800

    add BBA0Rule, bug: something brakes the auto switch of video size in all strategies.

--- src/streaming/rules/abr/BBA0Rule.js
+++ src/streaming/rules/abr/BBA0Rule.js
@@ -16,42 +16,111 @@ function BBA0Rule(config) {
     const dashMetrics = config.dashMetrics;
     const mediaPlayerModel = config.mediaPlayerModel;
     const eventBus = EventBus(context).getInstance();
-
-    let instance,
-        logger;
-
     const reservoir = 8;
     const cushion = 30;
     let ratePrev = 0;
 
-    function setup() {
-        logger = Debug(context).getInstance().getLogger(instance);
-        // eventBus.on(MediaPlayerEvents.BUFFER_EMPTY, onBufferEmpty, instance);
-        // eventBus.on(MediaPlayerEvents.PLAYBACK_SEEKING, onPlaybackSeeking, instance);
-        // eventBus.on(MediaPlayerEvents.METRIC_ADDED, onMetricAdded, instance);
-        // eventBus.on(MediaPlayerEvents.QUALITY_CHANGE_REQUESTED, onQualityChangeRequested, instance);
-        // eventBus.on(MediaPlayerEvents.FRAGMENT_LOADING_ABANDONED, onFragmentLoadingAbandoned, instance);
-        // eventBus.on(Events.MEDIA_FRAGMENT_LOADED, onMediaFragmentLoaded, instance);
+    function comp(propertyName) {
+        return function(object1, object2) {
+            var value1 = object1[propertyName];
+            var value2 = object2[propertyName];
+            return value1 - value2;
+        };
+    }
+
+    function fun(currentBufferLevel, step, rateMap) {
+        if (currentBufferLevel <= cushion + reservoir && currentBufferLevel >= reservoir)
+            return rateMap[Math.round((currentBufferLevel - reservoir) / step) * step + reservoir];
+
+        else if (currentBufferLevel > cushion + reservoir)
+            return rateMap[cushion + reservoir];
+
+        else
+            return rateMap[reservoir];
     }
 
     function getMaxIndex(rulesContext) {
+        const mediaInfo = rulesContext.getMediaInfo();
+        const mediaType = rulesContext.getMediaInfo().type;
+        const metrics = metricsModel.getMetricsFor(mediaType, true);
+        const abrController = rulesContext.getAbrController();
+        const switchRequest = SwitchRequest(context).create();
+
+        if (mediaType === 'video') {
+            let bitrateList = abrController.getBitrateList(mediaInfo);
+            bitrateList.sort(comp('bitrate'));
+            let rateMap = {};
+
+            let step = cushion / (bitrateList.length - 1);
+
+            for (let i = 0; i < bitrateList.length; i++)
+                rateMap[reservoir + i * step] = bitrateList[i].bitrate;
+
+            let rateMax = bitrateList[bitrateList.length - 1].bitrate;
+            let rateMin = bitrateList[0].bitrate;
+            ratePrev = ratePrev > rateMin ? ratePrev : rateMin;
+            let ratePlus = rateMax;
+            let rateMinus = rateMin;
+
+            if (ratePrev === rateMax)
+                ratePlus = rateMax;
+            else
+                for (let i = 0; i < bitrateList.length; i++)
+                    if (bitrateList[i].bitrate > ratePrev) {
+                        ratePlus = bitrateList[i].bitrate;
+                        break;
+                    }
+
+            if (ratePrev === rateMin)
+                rateMinus = rateMin;
+            else
+                for (let i = bitrateList.length - 1; i >= 0; i--)
+                    if (bitrateList[i].bitrate < ratePrev) {
+                        rateMinus = bitrateList[i].bitrate;
+                        break;
+                    }
+            let currentBufferLevel = dashMetrics.getCurrentBufferLevel(metrics);
+            let fCurrentBufferLevel = fun(currentBufferLevel, step, rateMap);
+
+            let rateNext;
+            if (currentBufferLevel <= reservoir)
+                rateNext = rateMin;
+            else if (currentBufferLevel >= reservoir + cushion)
+                rateNext = rateMax;
+            else if (fCurrentBufferLevel >= ratePlus) {
+                for (let i = bitrateList.length - 1; i >= 0; i--)
+                    if (bitrateList[i].bitrate <= fCurrentBufferLevel) {
+                        rateNext = bitrateList[i].bitrate;
+                        break;
+                    }
+            }
+            else if (fCurrentBufferLevel <= rateMinus) {
+                for (let i = 0; i < bitrateList.length; i++)
+                    if (bitrateList[i].bitrate > fCurrentBufferLevel) {
+                        rateNext = bitrateList[i].bitrate;
+                        break;
+                    }
+            }
+            else
+                rateNext = ratePrev;
+            ratePrev = rateNext;
+            switchRequest.quality = abrController.getQualityForBitrate(mediaInfo, rateNext / 1000, 0);
+
+        } else {
+            switchRequest.quality = 0;
+        }
+        return switchRequest;
     }
 
     function reset() {
-        // eventBus.off(MediaPlayerEvents.BUFFER_EMPTY, onBufferEmpty, instance);
-        // eventBus.off(MediaPlayerEvents.PLAYBACK_SEEKING, onPlaybackSeeking, instance);
-        // eventBus.off(MediaPlayerEvents.METRIC_ADDED, onMetricAdded, instance);
-        // eventBus.off(MediaPlayerEvents.QUALITY_CHANGE_REQUESTED, onQualityChangeRequested, instance);
-        // eventBus.off(MediaPlayerEvents.FRAGMENT_LOADING_ABANDONED, onFragmentLoadingAbandoned, instance);
-        // eventBus.off(Events.MEDIA_FRAGMENT_LOADED, onMediaFragmentLoaded, instance);
     }
 
-    instance = {
+    let instance = {
         getMaxIndex: getMaxIndex,
         reset: reset
     };
 
-    setup();
+    console.log('using BBA0Rule');
     return instance;
 }
 
